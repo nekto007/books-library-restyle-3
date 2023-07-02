b6e6eba375b2bc44d44afecd3c9d2f2148740644
igorkorobko <nekto.korobko@gmail.com>
2023-07-02 23:04:34 +0300

    Integrate livereload.

--- render_website.py
+++ render_website.py
@@ -1,22 +1,12 @@
 import json
-from http.server import HTTPServer, SimpleHTTPRequestHandler
+
 from jinja2 import Environment, FileSystemLoader, select_autoescape
+
 BOOKS_FILENAME = 'books.json'
+from livereload import Server
 
 
-def fetch_books_from_json(filename) -> list[dict]:
-    with open(filename, 'r', encoding='utf-8') as file:
-        books = json.loads(file.read())
-
-    for book in books:
-        book['image_src'] = book['image_src'].replace('\\', '/')
-        book['book_path'] = book['book_path'].replace('\\', '/')
-
-    return books
-
-
-def main():
-    # books = fetch_books_from_json(BOOKS_FILENAME)
+def on_reload():
     with open('books.json', 'r', encoding='UTF-8') as file:
         books = json.load(file)
     env = Environment(
@@ -35,6 +25,7 @@ def main():
 
 
 if __name__ == '__main__':
-    main()
-    server = HTTPServer(('0.0.0.0', 8000), SimpleHTTPRequestHandler)
-    server.serve_forever()
+    on_reload()
+    server = Server()
+    server.watch('template.html', on_reload)
+    server.serve(open_url_delay=1)
