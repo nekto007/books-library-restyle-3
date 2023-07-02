751176dd68228a029af39ca07c3c97dbec8b525d
igorkorobko <nekto.korobko@gmail.com>
2023-07-03 00:17:07 +0300

    Split books list by pages.

--- render_website.py
+++ render_website.py
@@ -1,27 +1,33 @@
 import json
-
+import os
+from math import ceil
+from more_itertools import ichunked
 from jinja2 import Environment, FileSystemLoader, select_autoescape
+from livereload import Server
 
 BOOKS_FILENAME = 'books.json'
-from livereload import Server
 
 
 def on_reload():
+    os.makedirs('pages', exist_ok=True)
+    books_on_page = 10
     with open('books.json', 'r', encoding='UTF-8') as file:
         books = json.load(file)
-    env = Environment(
-        loader=FileSystemLoader('.'),
-        autoescape=select_autoescape(['html', 'xml'])
-    )
-    template = env.get_template('template.html')
-    print(books[0])
-    rendered_page = template.render(
-        books=books,
-
-    )
-
-    with open('index.html', 'w', encoding="utf8") as file:
-        file.write(rendered_page)
+    for page, block_books in enumerate(ichunked(books, books_on_page), start=1):
+        env = Environment(
+            loader=FileSystemLoader('.'),
+            autoescape=select_autoescape(['html', 'xml'])
+        )
+        template = env.get_template('template.html')
+        print(books[0])
+        rendered_page = template.render(
+            books=block_books,
+            page=page,
+            max_pages=ceil(len(books) / books_on_page),
+        )
+        path_page = os.path.join('pages', f'index{str(page)}.html')
+        with open(path_page, 'w', encoding="utf8") as file:
+            file.write(rendered_page)
 
 
 if __name__ == '__main__':
